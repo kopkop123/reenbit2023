efe34e10c639a690de69ea34c11c6ba7da0dc80e
Denis Tsymbalenko <den.cimbal@gmail.com>
2023-03-13 14:46:42 +0200

    Added Hashrouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './routes/Home';
 import Character from './components/Character/Character';
 import './App.css';
 
 const App = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" exact={true} element={<Home />} />
                 <Route path="/character/:id" element={<Character />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
